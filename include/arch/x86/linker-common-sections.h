kartben/zephyr-without-images

net: Add generic network interface header
This will be used by the new network stack to relate a device to actual
network context, and used in the different layers (mac, ip ...).

Change-Id: I30c08fa975314544c36b71636fd9653d562891b3
Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

include/arch/x86/linker-common-sections.h
@@ -196,6 +196,16 @@ SECTIONS
 		KEXEC_PGALIGN_PAD(MMU_PAGE_SIZE)
 	} GROUP_LINK_IN(RAM)
 
+#ifdef CONFIG_NET_YAIP
+	SECTION_PROLOGUE(net_if, (OPTIONAL),)
+	{
+		__net_if_start = .;
+		*(".net_if.*")
+		KEEP(*(SORT_BY_NAME(".net_if*")))
+		__net_if_end = .;
+	} GROUP_LINK_IN(RAM)
+#endif
+
 	SECTION_PROLOGUE(_k_task_list, ALIGN(4), ALIGN(4))
 	{
 		_k_task_list_start = .;

stevenlin08/zephyr-nordic

include/arch/x86/linker-common-sections.h
@@ -163,6 +163,16 @@ SECTIONS
 		KEXEC_PGALIGN_PAD(MMU_PAGE_SIZE)
 	} GROUP_DATA_LINK_IN(RAM, ROM)
 
+#ifdef CONFIG_NET_YAIP
+	SECTION_DATA_PROLOGUE(net_if, (OPTIONAL),)
+	{
+		__net_if_start = .;
+		*(".net_if.*")
+		KEEP(*(SORT_BY_NAME(".net_if*")))
+		__net_if_end = .;
+	} GROUP_DATA_LINK_IN(RAM, ROM)
+#endif
+
 	SECTION_DATA_PROLOGUE(_k_task_list, (OPTIONAL),)
 	{
 		_k_task_list_start = .;